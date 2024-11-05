d6f8ee171af76631c320cd2549e413f1d6f18f02
nikhil <nikhilpokuri143@gmail.com>
2024-11-05 11:40:55 +0530

    version 2.0

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def homepage():
     return "You are in homepage"
+
+@app.route("/new-feature", methods=["POST"])
+def feature():
+    return "New Feature Added"
